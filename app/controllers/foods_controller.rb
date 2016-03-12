12d103e4538144c600df53a1cb823908ac18eebf
Quan Vu <nongdenchet8435848@gmail.com>
2016-03-12 09:00:15 +0700

    fix js rating

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id]).decorate
     @food.increase_views_count if params[:show]
-    @related_foods = Food.where(cuisine: @food.cuisine).take(2)
+    @related_foods = FoodDecorator.decorate_collection(Food.where(cuisine: @food.cuisine).take(2))
     @reviews = @food.reviews.order("id asc")
   end
 end
